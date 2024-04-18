bbda74677869249e0e38bc0f9bc0046862384e22
zh.kairatuly <zhanibek200353@gmail.com>
2024-04-18 11:15:25 +0600

    fix

--- src/main/java/kz/iitu/healthapp/controller/MedicationController.java
+++ src/main/java/kz/iitu/healthapp/controller/MedicationController.java
@@ -21,25 +21,25 @@ public class MedicationController {
 
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/{id}")
-    public ResponseEntity<Medication> getMedicationById(@PathVariable Long id) {
+    public ResponseEntity<?> getMedicationById(@PathVariable Long id) {
         Medication medication = medicationService.getById(id);
         return ResponseEntity.ok().body(medication);
     }
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/all")
-    public ResponseEntity<List<Medication>> getAllMedications() {
+    public ResponseEntity<?> getAllMedications() {
         List<Medication> medications = medicationService.getAllMedications();
         return ResponseEntity.ok().body(medications);
     }
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping("/create")
-    public ResponseEntity<Medication> createMedication(@RequestBody Medication medication) {
+    public ResponseEntity<?> createMedication(@RequestBody Medication medication) {
         Medication createdMedication = medicationService.createMedication(medication);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMedication);
     }
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @PutMapping("/{id}/update")
-    public ResponseEntity<Medication> updateMedication(@PathVariable Long id, @RequestBody Medication medication) {
+    public ResponseEntity<?> updateMedication(@PathVariable Long id, @RequestBody Medication medication) {
         Medication updatedMedication = medicationService.updateMedication(id, medication);
         return ResponseEntity.ok().body(updatedMedication);
     }
